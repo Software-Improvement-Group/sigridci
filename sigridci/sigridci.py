b1a67e981b1201b1c86051762b806cf729d88375
Arno Noordover <anoordover@users.noreply.github.com>
2022-02-08 12:59:41 +0100

    print cert

--- sigridci/sigridci.py
+++ sigridci/sigridci.py
@@ -29,6 +29,7 @@ import urllib.parse
 import urllib.request
 import ssl
 import zipfile
+import certifi
 
 LOG_HISTORY = []
 
@@ -100,8 +101,9 @@ class SigridApiClient:
         ctx = ssl.create_default_context()
         ctx.check_hostname = False
         ctx.verify_mode = ssl.CERT_NONE
-        # response = urllib.request.urlopen(request, context=ctx)
-        response = urllib.request.urlopen(request)
+        print(certifi.where())
+        response = urllib.request.urlopen(request, context=ctx)
+        # response = urllib.request.urlopen(request)
         if response.status == 204:
             return {}
         responseBody = response.read().decode("utf8")
@@ -161,8 +163,9 @@ class SigridApiClient:
             ctx = ssl.create_default_context()
             ctx.check_hostname = False
             ctx.verify_mode = ssl.CERT_NONE
-            # uploadResponse = urllib.request.urlopen(uploadRequest, context=ctx)
-            uploadResponse = urllib.request.urlopen(uploadRequest)
+            print(certifi.where())
+            uploadResponse = urllib.request.urlopen(uploadRequest, context=ctx)
+            # uploadResponse = urllib.request.urlopen(uploadRequest)
             return uploadResponse.status in [200, 201, 202]
             
     def checkSystemExists(self):
