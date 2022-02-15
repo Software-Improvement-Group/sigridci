8428048ff63352a5a6714b75d0831277c94857b6
Arno Noordover <anoordover@hetcak.nl>
2022-02-15 17:05:41 +0100

    added pem

--- sigridci/sigridci.py
+++ sigridci/sigridci.py
@@ -170,7 +170,7 @@ class SigridApiClient:
             ctx = ssl.create_default_context()
             ctx.check_hostname = False
             #ctx.verify_mode = ssl.CERT_NONE
-            ctx.load_verify_locations("sigridci/sigridci/sigrid-says.pem")
+            ctx.load_verify_locations("sigridci/sigridci/consolidated.pem")
             print (certifi.where())
             uploadResponse = urllib.request.urlopen(uploadRequest, context=ctx)
             # uploadResponse = urllib.request.urlopen(uploadRequest)
