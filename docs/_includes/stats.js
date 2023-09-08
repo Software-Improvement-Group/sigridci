7bc2dea64d9f3b8f8fe26846ad420cace8b65356
Dennis Bijlsma <d.bijlsma@sig.eu>
2023-09-08 08:49:45 +0200

    Explicitly set URL.

--- docs/_includes/stats.js
+++ docs/_includes/stats.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const _paq = window._paq = window._paq || [];
     _paq.push(["trackPageView"]);
     _paq.push(["enableLinkTracking"]);
+    _paq.push(["setCustomUrl", document.location.href]);
     _paq.push(["setTrackerUrl", "https://sigrid-says.com/usage/matomo.php"]);
     _paq.push(["setSiteId", "4"]);
 
